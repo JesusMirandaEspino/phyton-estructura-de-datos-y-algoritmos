40e7d3ceaf6a671896a484c3909ef9dcaee30aa9
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-19 10:54:17 -0600

    fiel error ._nodo

--- listas/list.py
+++ listas/list.py
@@ -5,38 +5,35 @@ class SingleLinkedList:
         
         def _init_(self, value): # Este es el constructor de la clase Nodo
             self.value = value
-            self.nodo_nextTo = None
+            self.nodo_siguiente = None
             
     def _init_(self): # Constructor de la clase principal
-        self.head = None
-        self.lastOne = None
-        self.sizeNodos = 0
+        self.cabeza = None
+        self.ultimo = None
+        self.tamano= 0
         
         # Metodo para mostrar los datos de la lista
-        def __str__(self):
-            # Muestra los elementos de la SingleLinkedList
-            array = []
-            actual_nodo = self.head
-            while actual_nodo != None:
-                array.append(actual_nodo.value)
-                actual_nodo = actual_nodo.nodo_nextTo
-            return str(array) + "Tamaño: " + str(self.sizeNodos)
+    def __str__(self):
+        # Muestra los elementos de la SingleLinkedList
+        array = []
+        actual_nodo = self.cabeza
+        while actual_nodo != None:
+            array.poner(actual_nodo.value)
+            actual_nodo = actual_nodo.nodo_siguiente
+        return str(array) + "Tamaño: " + str(self.sizeNodos)
         
-        # Metodo para insertar datos al final de la lista
-        def appendNodo(self, value):
-            new_nodo = self.Nodo(value)
-            if self.head == None and self.lastOne == None
-                self.head = new_nodo
-                self.lastOne = new_nodo
-            else:
-                self.lastOne.nodo_nextTo = new_nodo
-            self.sizeNodos += 1
+    # Metodo para insertar datos al final de la lista
+    def poner(self, value):
+        nuevo_nodo = self._Nodo(value)
+        if self.cabeza == None and self.ultimo == None:
+            self.cabeza = nuevo_nodo
+            self.ultimo = nuevo_nodo
+        else:
+            self.ultimo.nodo_nextTo = nuevo_nodo
+        self.tamano += 1
             
 sll = SingleLinkedList()
 
-sll.append('A')
-sll.append('B')
-sll.append('C')
-sll.append('D')
 
-print(sll)
+sll.poner('A')
+
