5e3e247c787869de96ead92ee0f0053e416dac4e
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-19 10:13:51 -0600

    Print the list method

--- listas/list.py
+++ listas/list.py
@@ -1,9 +1,23 @@
+#TODO Lista de datos
+
 class SingleLinkedList:
     class _Nodo: # Esta es una clase Privada por el _ guin bajo
+        
         def _init_(self, value): # Este es el constructor de la clase Nodo
             self.value = value
             self.nodo nextTo = None
+            
     def _init_(self): # Constructor de la clase principal
         self.head = None
         self.lastOne = None
         self.sizeNodos = 0
+        
+        # Metodo para mostrar los datos de la lista
+        def __str__(self):
+            # Muestra los elementos de la SingleLinkedList
+            array = []
+            actual_nodo = self.head
+            while actual_nodo != None:
+                array.append(actual_nodo.value)
+                actual_nodo = actual_nodo.nodo_nextTo
+            return str(array) + "Tamaño: " + str(self.sizeNodos)    
