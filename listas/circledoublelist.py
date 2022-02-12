49a05cbc9cbd880e260067d93edfb5f704d60d69
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-02-12 09:47:16 -0600

    Prepend method

--- listas/circledoublelist.py
+++ listas/circledoublelist.py
@@ -24,3 +24,20 @@ class circleDoubleLinkeList:
             else:
                 break
         return str(array) + "Tamaño: " + str(self.tamano)
+
+
+    # Agrega un elemento al principio de la listas
+    def prepend(self, valor):
+        nuevo_nodo = self._Nodo(valor)
+        if self.cabeza == None and self.cola == None:
+            self.cabeza = nuevo_nodo
+            self.cola = nuevo_nodo
+        else:
+            self.cabeza.nodo_anterior = nuevo_nodo
+            nuevo_nodo.nodo_siguiente = self.cabeza
+            self.cola.nodo_siguiente = nuevo_nodo
+            nuevo_nodo.nodo_anterior = self.cola
+            self.cabeza = nuevo_nodo
+        self.tamano += 1
+
+
