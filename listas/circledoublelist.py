0202b7da0b6d327b5a2981c30b8ca3dffe076a65
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-02-12 09:25:25 -0600

    init class doublecirclelist

---
+++ listas/circledoublelist.py
@@ -0,0 +1,11 @@
+class circleDoubleLinkeList:
+    class _Nodo:
+        def __init__(self, valor):
+            self.valor = valor
+            self.nodo_anterior = None
+            self.nodo_siguiente = None
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+        self.tamano = None
+        
