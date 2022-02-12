638b937ab97f58ed1b82e9f4c7f43e6ac8a9a5a4
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-02-12 09:38:45 -0600

    Imprimir lista

--- listas/circledoublelist.py
+++ listas/circledoublelist.py
@@ -8,4 +8,19 @@ class circleDoubleLinkeList:
         self.cabeza = None
         self.cola = None
         self.tamano = None
-        
+
+    # Muestra los elementos de la lista
+    def str(self):
+        array = []
+        nodo_actual = self.cabeza
+        pivote = True
+        contador = self.tamano
+        while contador != 0:
+            if pivote != False or nodo_actual != self.cabeza:
+                array.append(nodo_actual.valor)
+                nodo_actual = nodo_actual.nodo_siguiente
+                pivote = False
+                contador -= 1
+            else:
+                break
+        return str(array) + "Tamaño: " + str(self.tamano)
