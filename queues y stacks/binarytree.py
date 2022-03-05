1721db8d68f1db96e350bfb6bac4f292097845ae
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-03-05 08:59:52 -0600

    insert method binary tree

--- queues y stacks/binarytree.py
+++ queues y stacks/binarytree.py
@@ -1,5 +1,5 @@
 class binarytree:
-    class _Nodo:
+    class Nodo:
         def __init__(self, valor):
             self.valor = valor
             self.rama_izquierda = None
@@ -7,4 +7,35 @@ class binarytree:
     def __init__(self):
         self.raiz = None
         self.tamano = None
-        
+
+    def insert(self,valor):
+        nuevo_nodo = self.Nodo(valor)
+        if self.raiz == None:
+            self.raiz = nuevo_nodo
+        else:
+            def recorrer( valor, nodo ):
+                if valor == nodo.valor:
+                    return 'El nodo ya existe'
+                elif valor < nodo.valor:
+                    if nodo.rama_izquierda == None:
+                        nodo.rama_izquierda = nuevo_nodo
+                        return True
+                    else:
+                        return recorrer( valor, nodo.rama_izquierda )
+                elif valor > nodo.valor:
+                    if nodo.rama_derecha == None:
+                        nodo.rama_derecha = nuevo_nodo
+                        return True
+                    else: 
+                        return recorrer( valor, nodo.rama_derecha )
+            recorrer( valor, self.raiz)
+                
+bst = binarytree()
+
+bst.insert(45)
+bst.insert(55)
+bst.insert(65)
+bst.insert(85)
+
+
+bst.insert(45)
