62b90852c2db18436c0c2959ee9f0ba9bc40a8b3
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-03-19 09:08:34 -0600

    sort list

---
+++ algoritmos/sort.py
@@ -0,0 +1,16 @@
+def selectionsort(lista):
+    for i in range(len(lista)):
+        indice_mas_pequeno = i
+        for j in range( i + 1, len(lista) ):
+            if lista[j] < lista[indice_mas_pequeno]:
+                indice_mas_pequeno = j
+        if i != indice_mas_pequeno:
+            elemento_temporal = lista[i]
+            lista[i] = lista[ indice_mas_pequeno ]
+            lista[indice_mas_pequeno] = elemento_temporal
+    return print(lista)
+
+
+list = [ 1,5,7,9,3,8,2,6 ]
+
+selectionsort(list)
